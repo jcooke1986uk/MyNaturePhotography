f26ebc802b207c01ee56975e9b2d854ed3e3086b
jcooke1986uk <jcooke.web@gmail.com>
2019-08-11 20:40:48 +0100

    Doc build

---
+++ docs/build/precache-manifest.69aab53971bdc4798a17ee593746f30d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269185b0fa5a59cbd2aa9fa1991d4c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f004c9f54f78f46b7a1",
+    "url": "/static/css/main.6c47decb.chunk.css"
+  },
+  {
+    "revision": "a48adbc297338445e2f0",
+    "url": "/static/js/2.d99b1e42.chunk.js"
+  },
+  {
+    "revision": "9f004c9f54f78f46b7a1",
+    "url": "/static/js/main.dbd45257.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
